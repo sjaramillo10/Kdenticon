2f113d67718d560dbb8e3c2bf8a13c430c11d05c
Severiano Jaramillo <severiano.jaramillo@gmail.com>
2019-08-05 12:49:58 -0500

    Correctly manage configuration changes.
    - Configuration changes such as device rotation can cause an unintended behavior, for that reason the onSaveInstanceState() and onRestoreInstanceState() methods were added. These methods help to save and restore the KdenticonView coniguration.

--- library/src/main/java/dev/sjaramillo/kdenticon/KdenticonView.kt
+++ library/src/main/java/dev/sjaramillo/kdenticon/KdenticonView.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.widget.ImageView
 import jdenticon.Jdenticon
 import android.graphics.drawable.PictureDrawable
+import android.os.Bundle
+import android.os.Parcelable
 import android.util.AttributeSet
 import com.caverock.androidsvg.SVG
 
@@ -13,13 +15,20 @@ import com.caverock.androidsvg.SVG
 class KdenticonView(context: Context, attrs: AttributeSet) : ImageView(context, attrs) {
 
     companion object {
+        // Default values
         private const val DEFAULT_TEXT = ""
+        private const val DEFAULT_HASH_ALGO = 0 // MD5
+
+        // Constants used to correctly restore state
+        private const val KEY_TEXT = "key_text"
+        private const val KEY_HASH_ALGO = "key_hash_algo"
+        private const val KEY_SUPER_STATE = "key_super_state"
     }
 
     /**
      * Algorithm to be used to hash the given text.
      */
-    var hashAlgo = HashAlgos.MD5.ordinal
+    var hashAlgo = DEFAULT_HASH_ALGO
         set(value) {
             field = value
 
@@ -77,5 +86,25 @@ class KdenticonView(context: Context, attrs: AttributeSet) : ImageView(context, 
         val svg = SVG.getFromString(svgString)
         val pd = PictureDrawable(svg.renderToPicture())
         setImageDrawable(pd)
+    }
+
+    override fun onSaveInstanceState(): Parcelable? {
+        val bundle = Bundle()
+        bundle.putString(KEY_TEXT, text)
+        bundle.putInt(KEY_HASH_ALGO, hashAlgo)
+        bundle.putParcelable(KEY_SUPER_STATE, super.onSaveInstanceState())
+        return bundle
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        var viewState = state
+        if (viewState is Bundle) {
+            text = viewState.getString(KEY_TEXT, DEFAULT_TEXT)
+            hashAlgo = viewState.getInt(KEY_HASH_ALGO, DEFAULT_HASH_ALGO)
+
+            viewState = viewState.getParcelable(KEY_SUPER_STATE)
+        }
+
+        super.onRestoreInstanceState(viewState)
     }
 }
